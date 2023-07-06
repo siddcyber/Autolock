b2ccc9c160c6c45510331237835b9a608a014897
siddcyber <siddhantgulia@gmail.com>
2023-07-06 15:14:16 +0530

    creating a basic UI for the program inlcuding multiple functions and buttons

--- gui.py
+++ gui.py
@@ -1,55 +1,31 @@
-import cv2
-import tkinter as tk
-from tkinter import messagebox
-from PIL import Image, ImageTk
+from tkinter import *
 
-# Create a tkinter window
-window = tk.Tk()
-window.title("Face Detection")
 
-# Open the webcam
-cap = cv2.VideoCapture(0)
+def checkstartup():
+    return True
 
-# Load the pre-trained face detection cascade classifier
-face_cascade = cv2.CascadeClassifier(
-    'C:\Program Files\Python39\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
-profile_cascade = cv2.CascadeClassifier(
-    'C:\Program Files\Python39\Lib\site-packages\cv2\data\haarcascade_profileface.xml')
+def checkCamera():
+    return True
 
-# Function to detect faces and display messagebox
-def detect_faces():
-    # Read a frame from the webcam
-    ret, frame = cap.read()
 
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+window = Tk()
+window.geometry("700x360")
+window.configure(background="White")
+window.title("Face Recognition Autolock")
+heading = Label(window, text="Face Recognition Autolock", relief='raised', bg="White")
 
-    # Detect faces in the frame using the cascade classifier
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+optionFrame = LabelFrame(window, background='white')
+startupButton = Button(optionFrame, text='Startup', command=checkstartup, background='White')
+showCameraButton = Button(optionFrame, text='Show camera', command=checkCamera, background='White')
 
-    # Display the frame in the tkinter window
-    img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    img = img.resize((400, 300))
-    imgtk = ImageTk.PhotoImage(image=img)
-    label.configure(image=imgtk)
-    label.image = imgtk
 
-    # Check if faces are detected
-    if len(faces) > 0:
-        messagebox.showinfo("Face Detected", "A face has been detected!")
+window.rowconfigure(index=3, weight=1)
+window.columnconfigure(index=0, weight=1)
 
-    # Schedule the next detection
-    window.after(10, detect_faces)
 
-# Create a label to display the webcam feed
-label = tk.Label(window)
-label.pack()
+heading.grid(row=0,column=0,sticky='NSEW')
+optionFrame.grid(row=1,column=0,sticky='NSEW')
+startupButton.grid(row=0,column=0,sticky='EW')
+showCameraButton.grid(row=0,column=1,sticky='EW')
 
-# Start the face detection process
-window.after(10, detect_faces)
-
-# Start the tkinter event loop
 window.mainloop()
-
-# Release the webcam
-cap.release()
